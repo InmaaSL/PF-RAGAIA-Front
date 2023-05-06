3bae4911a5270cff99fb3705c51749bb320a3d52
Inma Serrano <inmaserrano.daw@gmail.com>
2023-05-06 19:49:06 +0200

    Componentes login y reset password

---
+++ src/app/components/login-components/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+
+export class LoginComponent  implements OnInit {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.compose([Validators.email, Validators.required])),
+    password: new FormControl('', Validators.required),
+  });
+
+  guardarSesion = new FormControl();
+  alertErrorLogin = false;
+
+
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+
+  ) { }
+
+  ngOnInit() {
+  }
+
+  async login() {
+
+    this.auth.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      complete: () => {
+        if (this.guardarSesion.value) {
+          localStorage.setItem('guardarSesion', 'true');
+        } else {
+          localStorage.setItem('guardarSesion', 'false');
+        }
+
+        this.router.navigateByUrl('/home');
+      },
+      error: (e) => {
+        console.log(e);
+        this.alertErrorLogin = true;
+      }
+    });
+  }
+
+
+
+}
