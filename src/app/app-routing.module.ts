5bcee720c1ca7062fa78975d087534d3cf4871c5
Inma Serrano <inmaserrano.daw@gmail.com>
2023-05-06 20:06:58 +0200

    CanActive: control de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canActivate: [ AuthGuardService ]
   },
 ];
 
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,56 +1,24 @@
 import { Injectable } from '@angular/core';
-import { AuthenticationService } from './authentication.service';
 import { ActivatedRouteSnapshot, Router } from '@angular/router';
-
+import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService {
 
   constructor(
-    public auth: AuthenticationService,
+    public auth: AuthService,
     public router: Router
   ) { }
 
 
-  canActivate(route: ActivatedRouteSnapshot): boolean /*Observable<boolean>*/ {
-    const token = localStorage.getItem('_cap_jwt-token');
+  canActivate(): boolean /*Observable<boolean>*/ {
+    const token = localStorage.getItem('jwt-token');
     if (token) {
       return true;
     }
-    this.router.navigateByUrl('login');
+    this.router.navigateByUrl('/login');
     return false;
-
-
-    // const token = JSON.parse(localStorage.getItem('jwt-token'));
-    // if (token) {
-    //   this.rolesUrl = route.data?.roles;
-
-    //   if(this.rolesUrl){
-    //     this.rolesUser =  this.auth.getUserRoles();
-
-    //     if(!this.rolesUser){
-    //       this.router.navigateByUrl('/login');
-    //       return false;
-    //     }
-
-    //     let access = false;
-    //     this.rolesUrl.forEach(rol => {
-    //       if(!access){
-    //         access = this.rolesUser.includes(rol);
-    //       }
-    //     });
-
-    //     if(access){
-    //       return true;
-    //     }
-    //     this.router.navigateByUrl('/');
-    //     return false;
-    //   }
-    // }
-    // this.router.navigateByUrl('/login');
-    // return false;
-
   }
 
 }
